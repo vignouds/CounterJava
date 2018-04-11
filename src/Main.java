69068e0964689103d26b48584714d9f7ac5c46de
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-11 11:24:27 +0200

    test des methodes getValue et inc via main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
 	public static void main(String[] args) {
-		
+		Counter counter = new Counter();
+		counter.getValue();
+		System.out.println(counter.getValue());
+		counter.inc();
+		System.out.println(counter.getValue());
 	}
 }
