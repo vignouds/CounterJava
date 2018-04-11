54af7573309b88588c8c3896a4fbb23055b99cc4
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-11 11:15:40 +0200

    creation methode getValue

--- src/Counter.java
+++ src/Counter.java
@@ -2,15 +2,22 @@
 class Counter {
 	int position, step;
 	
+	//Constructeur "vide"
 	Counter() 
 	{
 		position=0;
 		step=1;
 	}
 	
+	// Constructeur avec paramètres
 	Counter(int p, int s)
 	{
 		position=p;
 		step=s;
 	}
+	
+	public int getValue(){
+		return position;
+	}
+	
 }
