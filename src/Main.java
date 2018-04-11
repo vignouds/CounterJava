db9948a2df6cf87bbc26dcd16e186e03dce40d47
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-11 11:44:56 +0200

    ajout de quelques lignes dans fonction main pour les tests

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@
 public class Main {
 	public static void main(String[] args) {
 		Counter counter = new Counter();
-		Counter counter2 = new Counter();
+		Counter counter2 = new Counter(0,2);
 		System.out.println(counter.getValue());
 		counter.inc();
 		counter2.inc();
 		System.out.println(counter.getValue());
 		System.out.println(counter.toString());
+		System.out.println(counter2.toString());
 		System.out.println(counter.equals(counter2));
 	}
 }
