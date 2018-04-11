fcfec5b209374157dd231faf4b95ed05bcc22280
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-04-11 14:32:08 +0200

    optimisation du code avec le prof

--- src/Counter.java
+++ src/Counter.java
@@ -1,28 +1,22 @@
-
 class Counter {
 	private int position, step;
 	private static int globalCounter=-1;
-	
-	//Constructeur "vide"
-	public Counter() 
-	{
-		this.position=0;
-		this.step=1;
-		
-		if(globalCounter==-1) {
-			globalCounter=0;
-		}
-	}
-	
+
 	// Constructeur avec paramètres
 	public Counter(int p, int s)
 	{
 		this.position=p;
 		this.step=s;
 		
-		if(globalCounter!=-1) {
+		if(globalCounter==-1) {
 			globalCounter=p;
 		}
+	}
+	
+		//Constructeur "vide"
+	public Counter() 
+	{
+		this(0,1);
 	}
 	
 	public int getValue(){
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-
 public class Main {
 	public static void main(String[] args) {
 		Counter counter1 = new Counter();
-		Counter counter2=counter1;
+		Counter counter2 = new Counter(0,1);
+		System.out.println("Compteur global avant incrémentation : " + Counter.getGlobalCounter());
 		counter1.inc();
+		System.out.println("Compteur global : " + Counter.getGlobalCounter());
 		counter1.inc();
+		System.out.println("Compteur global : " + Counter.getGlobalCounter());
 		counter1.inc();
+		System.out.println("Compteur global : " + Counter.getGlobalCounter());
 		counter2.inc();
 		System.out.println(counter1.toString());
 		System.out.println(counter2.toString());
 		System.out.println(counter1.equals(counter2));
-		System.out.println(Counter.getGlobalCounter());
+		System.out.println("Compteur global : " + Counter.getGlobalCounter());
 	}
 }
